af40539d82ffd0c3e16c45eddd7f31a4fff50a6f
Mike Caine <mikey.caine@btinternet.com>
2022-05-02 11:20:44 +0100

    init

---
+++ src/test/java/com/mikeycaine/ingest/IngestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikeycaine.ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
