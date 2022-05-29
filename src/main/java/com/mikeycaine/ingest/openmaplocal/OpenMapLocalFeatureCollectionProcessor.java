656c91c4b33c349347640974f53cad4144b07517
Mike Caine <mikey.caine@btinternet.com>
2022-05-29 20:13:23 +0100

    openmaplocal

---
+++ src/main/java/com/mikeycaine/ingest/openmaplocal/OpenMapLocalFeatureCollectionProcessor.java
@@ -0,0 +1,26 @@
+package com.mikeycaine.ingest.openmaplocal;
+
+import lombok.extern.slf4j.Slf4j;
+
+import net.opengis.gml._3.AbstractFeatureType;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Service;
+import uk.os.namespaces.product._1.FeatureCollectionType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class OpenMapLocalFeatureCollectionProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Message in = exchange.getIn();
+        FeatureCollectionType fct = in.getBody(FeatureCollectionType.class);
+
+        List<AbstractFeatureType> featureList = fct.getFeatureMember().stream().map(fpt -> fpt.getAbstractFeature().getValue()).collect(Collectors.toList());
+        in.setBody(featureList, List.class);
+    }
+}
--- src/main/java/com/mikeycaine/ingest/openmaplocal/OpenMapLocalRoutes.java
+++ src/main/java/com/mikeycaine/ingest/openmaplocal/OpenMapLocalRoutes.java
@@ -12,12 +12,13 @@ import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
 import org.springframework.stereotype.Component;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 @Component
 @RequiredArgsConstructor
 public class OpenMapLocalRoutes extends RouteBuilder {
 
-//    private final WatercourseLinkProcessor watercourseLinkProcessor;
+    private final OpenMapLocalFeatureCollectionProcessor openMapLocalFeatureCollectionProcessor;
 //    private final HydroNodeProcessor hydroNodeProcessor;
 
     final static String OPENMAPLOCAL_DATA = "E:\\Downloads\\opmplc_gml3_gb\\data";
@@ -25,58 +26,92 @@ public class OpenMapLocalRoutes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:" + OPENMAPLOCAL_DATA + "?noop=true&recursive=true")
-            .routeId("openmaplocal unzip")
+            .routeId("openmaplocal")
             .autoStartup(true)
-            .idempotentConsumer(header("CamelFileName"),
-                MemoryIdempotentRepository.memoryIdempotentRepository(10000))
-            .log("Unzipping ${file:path}");
+//            .idempotentConsumer(header("CamelFileName"),
+//                MemoryIdempotentRepository.memoryIdempotentRepository(1000))
+            .log("OpenMapLocal ${file:path}")
+            .unmarshal(jaxbDataFormat())
+            .process(openMapLocalFeatureCollectionProcessor)
+            .split(body())
+            .streaming()
+            .choice()
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.BuildingType'"))
+                    .to("direct:buildings")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.SurfaceWaterLineType'"))
+                    .to("direct:surfacewaterline")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.TidalBoundaryType'"))
+                    .to("direct:tidalboundary")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.WoodlandType'"))
+                    .to("direct:woodland")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.RoadType'"))
+                    .to("direct:road")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.SurfaceWaterAreaType'"))
+                    .to("direct:surfacewaterarea")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.TidalWaterType'"))
+                    .to("direct:tidalwater")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.ForeshoreType'"))
+                    .to("direct:foreshore")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.NamedPlaceType'"))
+                    .to("direct:namedplace")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.CarChargingPointType'"))
+                    .to("direct:carchargingpoint")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.FunctionalSiteType'"))
+                    .to("direct:functionalsite")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.ImportantBuildingType'"))
+                    .to("direct:importantbuilding")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.RailwayTrackType'"))
+                    .to("direct:railwaytrack")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.RoundaboutType'"))
+                    .to("direct:roundabout")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.ElectricityTransmissionLineType'"))
+                    .to("direct:electricitytransmissionline")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.RailwayStationType'"))
+                    .to("direct:railwaystation")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.RailwayTunnelType'"))
+                    .to("direct:railwaytunnel")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.RoadTunnelType'"))
+                    .to("direct:roadtunnel")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.GlasshouseType'"))
+                    .to("direct:glasshouse")
+                .when(simple("${body} is 'uk.os.namespaces.open.oml._1.MotorwayJunctionType'"))
+                    .to("direct:motorwayjunction")
+                .otherwise()
+                    .to("direct:unknown");
 
+        from("direct:buildings").stop();
+        from("direct:surfacewaterline").stop();
+        from("direct:tidalboundary").stop();
+        from("direct:woodland").stop();
+        from("direct:road").stop();
+        from("direct:surfacewaterarea").stop();
+        from("direct:tidalwater").stop();
+        from("direct:foreshore").stop();
+        from("direct:namedplace").stop();
+        from("direct:carchargingpoint").stop();
+        from("direct:functionalsite").stop();
+        from("direct:importantbuilding").stop();
+        from("direct:railwaytrack").stop();
+        from("direct:roundabout").stop();
+        from("direct:electricitytransmissionline").stop();
+        from("direct:railwaystation").stop();
+        from("direct:railwaytunnel").stop();
+        from("direct:roadtunnel").stop();
+        from("direct:glasshouse").stop();
+        from("direct:motorwayjunction").stop();
 
+        from("direct:unknown").to("log:unknown");
+    }
 
+    private JaxbDataFormat jaxbDataFormat() throws JAXBException {
+        JaxbDataFormat jaxbDataFormat = new JaxbDataFormat();
 
-//
-//            .split(new ZipSplitter())
-//            .streaming()
-//            .log("Unzipped ${file:name}")
-//            .end();
-//            .to("direct:terrain50data");
+        JAXBContext jaxbContext = JAXBContext.newInstance(
+            uk.os.namespaces.open.oml._1.ObjectFactory.class,
+            uk.os.namespaces.product._1.ObjectFactory.class
+        );
 
-//        JaxbDataFormat jaxbDataFormat = new JaxbDataFormat();
-//
-//        JAXBContext jaxbContext = JAXBContext.newInstance(
-//            uk.os.namespaces.open.rivers._1.ObjectFactory.class,
-//            inspire.x.specification.gmlas.hydronetwork._3.ObjectFactory.class,
-//            uk.os.namespaces.open.rivers._1.WatercourseLinkType.class,
-//            uk.os.namespaces.product._1.ObjectFactory.class,
-//            net.opengis.gml._3.ObjectFactory.class
-//        );
-//
-//        jaxbDataFormat.setContext(jaxbContext);
-//
-//        Namespaces namespaces = new Namespaces()
-//            .add("os", "http://namespaces.os.uk/product/1.0")
-//            .add("river", "http://namespaces.os.uk/Open/Rivers/1.0");
-//
-//        from("file:" + OPENRIVERS_DATA + "&noop=true")
-//            .multicast()
-//            .to("seda:watercourselinks", "seda:hydronodes");
-//
-//        from("seda:watercourselinks")
-//            .routeId("watercourselinks")
-//            .autoStartup(false)
-//            .split(xpath("//river:WatercourseLink", namespaces))
-//            .streaming()
-//            .unmarshal(jaxbDataFormat)
-//            .process(watercourseLinkProcessor)
-//            .to("jpa:WatercourseLink");
-//
-//        from("seda:hydronodes")
-//            .routeId("hydronodes")
-//            .autoStartup(false)
-//            .split(xpath("//river:HydroNode", namespaces))
-//            .streaming()
-//            .unmarshal(jaxbDataFormat)
-//            .process(hydroNodeProcessor)
-//            .to("jpa:HydroNode");
+        jaxbDataFormat.setContext(jaxbContext);
+        return jaxbDataFormat;
     }
 }
