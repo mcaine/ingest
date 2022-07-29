82f2dbf44addb51c955f742016d7896dfc8e7b40
Mike Caine <mikey.caine@btinternet.com>
2022-07-29 11:56:57 +0100

    use 1D array to store terrain heights

--- src/main/java/com/mikeycaine/ingest/terrain50/Terrain50Grid.java
+++ src/main/java/com/mikeycaine/ingest/terrain50/Terrain50Grid.java
@@ -12,6 +12,11 @@ import java.util.Scanner;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+// Found 2858 terrain tiles
+// Max height for tile NN17: HeightAtPosition(x=216650, y=771250, height=1345.0, gridSquare=NN17)
+// Max height for tile NY20: HeightAtPosition(x=321500, y=507200, height=974.2, gridSquare=NY20)
+// Max height for tile SH65: HeightAtPosition(x=260950, y=354350, height=1080.4, gridSquare=SH65)
+
 @Entity
 @Table(name="terrain50grid")
 public class Terrain50Grid {
@@ -42,12 +47,12 @@ public class Terrain50Grid {
 
     @Getter
     @Setter
-    private float[][] terrainData;
+    private float[] terrainData;
 
     public Terrain50Grid() {
     }
 
-    public Terrain50Grid(String gridSquare, int nRows, int nCols, int xllCorner, int yllCorner, int cellSize, float[][] terrainData) {
+    public Terrain50Grid(String gridSquare, int nRows, int nCols, int xllCorner, int yllCorner, int cellSize, float[] terrainData) {
         this.gridSquare = gridSquare;
         this.nRows = nRows;
         this.nCols = nCols;
@@ -64,8 +69,9 @@ public class Terrain50Grid {
         int yllCorner = 0;
         int cellSize = 0;
         int rowIdx = 0;
+        int dataIdx = 0;
 
-        float[][] data = null;
+        float[] data = null;
 
         try (Scanner scanner = new Scanner(ascString)) {
             while (scanner.hasNext()) {
@@ -83,11 +89,19 @@ public class Terrain50Grid {
                 } else {
                     if (!line.isEmpty() && nCols > 0 && nRows > 0) {
                         if (data == null) {
-                            data = new float[nRows][nCols];
+                            data = new float[nRows * nCols];
                         }
+
                         Scanner dataScanner = new Scanner(line);
-                        for (int colIdx = 0; colIdx < nCols; ++colIdx) {
-                            data[rowIdx][colIdx] = dataScanner.nextFloat();
+                        int itemsInThisRow = 0;
+                        while(dataScanner.hasNextFloat()) {
+                            float h = dataScanner.nextFloat();
+                            ++itemsInThisRow;
+                            data[dataIdx] = h;
+                            ++dataIdx;
+                        }
+                        if (itemsInThisRow != nCols) {
+                            throw new RuntimeException("Expected " + nCols + " items in row, found " + itemsInThisRow);
                         }
                     }
                     ++rowIdx;
@@ -105,8 +119,11 @@ public class Terrain50Grid {
     public Stream<HeightAtPosition> heights() {
         return IntStream.range(0, nRows).mapToObj(Integer::valueOf)
                 .flatMap(rowIdx -> IntStream.range(0, nCols).mapToObj(
-                    colIdx -> new HeightAtPosition(xllCorner + colIdx * cellsize, yllCorner + (nRows - rowIdx - 1) * cellsize, terrainData[rowIdx][colIdx], gridSquare))
-                );
+                    colIdx -> {
+                        int idx = rowIdx * nCols + colIdx;
+                        return new HeightAtPosition(xllCorner + colIdx * cellsize, yllCorner + (nRows - rowIdx - 1) * cellsize, terrainData[idx], gridSquare);
+                    }
+                ));
     }
 
     public Optional<HeightAtPosition> maxHeightOpt() {
