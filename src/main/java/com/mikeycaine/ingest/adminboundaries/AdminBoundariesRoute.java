a889e14123dc54631b291deb3e6991fb4fc6a8e2
Mike Caine <mikey.caine@btinternet.com>
2025-08-08 13:19:29 +0100

    tidy up AdminBoundariesRoute

--- src/main/java/com/mikeycaine/ingest/adminboundaries/AdminBoundariesRoute.java
+++ src/main/java/com/mikeycaine/ingest/adminboundaries/AdminBoundariesRoute.java
@@ -1,17 +1,11 @@
 package com.mikeycaine.ingest.adminboundaries;
 
-import com.mikeycaine.ingest.adminboundaries.AdministrativeUnitProcessor;
 import jakarta.xml.bind.JAXBContext;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.support.builder.Namespaces;
 import org.springframework.stereotype.Component;
-
-import eu.europa.ec.inspire.schemas.au._4.AdministrativeUnitType;
-
-
-
 
 @Component
 @RequiredArgsConstructor
@@ -23,21 +17,18 @@ public class AdminBoundariesRoute extends RouteBuilder {
     public void configure() throws Exception {
 
         JaxbDataFormat jaxbDataFormat = new JaxbDataFormat();
-        JAXBContext jaxbContext = JAXBContext.newInstance("eu.europa.ec.inspire.schemas.au._4");
-        jaxbDataFormat.setContext(jaxbContext);
+        jaxbDataFormat.setContext(JAXBContext.newInstance("eu.europa.ec.inspire.schemas.au._4"));
 
         Namespaces namespaces = new Namespaces()
                 .add("au", "http://inspire.ec.europa.eu/schemas/au/4.0");
 
         from("file:E:\\Downloads\\bdline_gml3_gb\\Data?fileName=INSPIRE_AdministrativeUnit.gml&noop=true")
-                .routeId("admin boundaries ingest")
-                .autoStartup(true)
-                .split(xpath("//au:AdministrativeUnit", namespaces))
+             .routeId("AdministrativeUnit")
+             .autoStartup(true)
+             .split(xpath("//au:AdministrativeUnit", namespaces))
             .streaming()
             .unmarshal(jaxbDataFormat)
             .process(administrativeUnitProcessor)
             .end();
     }
-
-
 }
