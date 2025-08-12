5e53ef5abb692673fc710acf8a8214b6b8e3fc2c
Mike Caine <mikey.caine@btinternet.com>
2025-08-12 12:21:50 +0100

    tidying

--- src/main/java/com/mikeycaine/ingest/opentoid/OSOpenTOID.java
+++ src/main/java/com/mikeycaine/ingest/opentoid/OSOpenTOID.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
